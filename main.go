ceef528bc45dfbab9fcd472de9cc986c7f81cead
mattbw <matt.windsor@ury.york.ac.uk>
2015-07-06 17:40:47 +0100

    Start separating out trackd-specific code.

--- main.go
+++ main.go
@@ -33,7 +33,34 @@ Options:
 		log.Fatal(err)
 	}
 
-	ln, err := net.Listen("tcp", arguments["HOSTPORT"].(string))
+	db, err := getDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	t := NewTrackDB(db, `M:\%d\%d`)
+
+	RunTcpServer(RequestMap{
+		baps3.RqRead: func(b, r chan<- *baps3.Message, s []string) (bool, error) { return handleRead(b, r, t, s) },
+		baps3.RqQuit: func(_, _ chan<- *baps3.Message, _ []string) (bool, error) { return true, nil },
+	}, arguments["HOSTPORT"].(string))
+}
+
+// RequestHandler is the type of handlers added to a RequestMap.
+type RequestHandler func(chan<- *baps3.Message, chan<- *baps3.Message, []string) (bool, error)
+
+// RequestMap is a map from requests (as message words) to RequestHandlers.
+type RequestMap map[baps3.MessageWord]RequestHandler
+
+// RunTcpServer creates and runs a Bifrost server using TCP as a transport.
+// It will respond to requests using the functions in requestMap
+func RunTcpServer(requestMap RequestMap, hostport string) {
+	ln, err := net.Listen("tcp", hostport)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,7 +84,7 @@ Options:
 
 	go AcceptLoop(ln, requests, &clientPoolHandle, &wg)
 
-	RequestLoop(requests, clientPoolHandle.Broadcast, &wg)
+	RequestLoop(requests, clientPoolHandle.Broadcast, requestMap, &wg)
 	log.Println("main loop closing")
 
 	// The client pool will tell all the connected clients to quit.
@@ -92,19 +119,7 @@ func AcceptLoop(ln net.Listener, requests chan<- *Request, clientPoolHandle *Cli
 	}
 }
 
-func RequestLoop(requests <-chan *Request, broadcast chan<- *baps3.Message, wg *sync.WaitGroup) {
-	db, err := getDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	t := NewTrackDB(db, `M:\%d\%d`)
-
+func RequestLoop(requests <-chan *Request, broadcast chan<- *baps3.Message, requestMap RequestMap, wg *sync.WaitGroup) {
 	for {
 		select {
 		case r, more := <-requests:
@@ -112,35 +127,23 @@ func RequestLoop(requests <-chan *Request, broadcast chan<- *baps3.Message, wg *
 				return
 			}
 			log.Printf("received request: %q", r.contents)
-			if finished := handleRequest(broadcast, t, r); finished {
+			if finished := handleRequest(broadcast, requestMap, r); finished {
 				return
 			}
 		}
 	}
 }
 
-type CmdTable map[baps3.MessageWord]func(chan<- *baps3.Message, chan<- *baps3.Message, *TrackDB, []string) (bool, error)
-
-var cmds CmdTable = CmdTable{
-	baps3.RqRead: handleRead,
-	baps3.RqQuit: handleQuit,
-}
-
-func handleQuit(_, _ chan<- *baps3.Message, _ *TrackDB, _ []string) (bool, error) {
-	return true, nil
-}
-
-func handleRequest(broadcast chan<- *baps3.Message, t *TrackDB, request *Request) bool {
+func handleRequest(broadcast chan<- *baps3.Message, requestMap RequestMap, request *Request) bool {
 	var lerr error
 	finished := false
 
 	msg := request.contents
 
-	// TODO: handle quit
 	// TODO: handle bad command
-	cmdfunc, ok := cmds[msg.Word()]
+	cmdfunc, ok := requestMap[msg.Word()]
 	if ok {
-		finished, lerr = cmdfunc(broadcast, request.response, t, msg.Args())
+		finished, lerr = cmdfunc(broadcast, request.response, msg.Args())
 	} else {
 		lerr = fmt.Errorf("FIXME: unknown command %q", msg.Word())
 	}
